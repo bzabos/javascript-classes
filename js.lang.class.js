5c6472ac756b371bba4453170b93592a939f4f54
b.zabos <b.zabos@ba2524b2-4777-11de-b695-a305669b6508>
2009-09-23 04:17:52 +0000

    - added docstrings to js.lang.class - swapped a couple (a === undefined)'s with Boolean(a)'s to check for cases of false, null, etc
    git-svn-id: https://javascript-classes.googlecode.com/svn/trunk@9 ba2524b2-4777-11de-b695-a305669b6508

--- js.lang.class.js
+++ js.lang.class.js
@@ -42,6 +42,7 @@ js.util.objectMergeAll = function(dest, srcs) {
 
 // global logger
 js.log = function(msg) {
+    /** Append given message to list of log-items.  **/
     js.log.appendLog(msg);
 
     if (js.settings.debug) {
@@ -52,19 +53,24 @@ js.log = function(msg) {
 js.log._logItems = [];
 
 js.log.appendLog = function(msg) {
-    var logItem = {};
-    logItem[String(new Date())] = String(msg);
+    /** Append an object representing a log-item to the log. **/
+    var logItem = {
+            message :   String(msg),
+            timestamp : String(new Date())
+        };
 
     js.log._logItems.push(logItem);
 };
 
 js.log._logger = (function(console, print) {
-    if ((console !== undefined) && ('log' in console)) {
+    /** Simple logger to wrap console functionality when swapping between
+        ss-js and dom. **/
+    if (Boolean(console) && ('log' in console)) {
         return (function(msg) {
             console.log(msg);
         });
     }
-    else if ((print !== undefined) && (print.constructor === Function)) {
+    else if (Boolean(print) && (print.constructor === Function)) {
         return (print);
     }
     return (function() {});
@@ -72,35 +78,36 @@ js.log._logger = (function(console, print) {
 
 js.lang = (js.lang || {});
 
-// reference to global context (in a browser: window)
 js.lang.GlobalContext = (function() {
+    /** Return a reference to global context. (in a browser: window) **/
     return (this);
 })();
 
-// consumer settings/preferences
 js.settings = {
+    /** Client settings/preferences. **/
     debug:           (js.lang.GlobalContext.JS_DEBUG            || false),
     globalClass:     (js.lang.GlobalContext.JS_GLOBAL_CLASS     || false),
     globalInterface: (js.lang.GlobalContext.JS_GLOBAL_INTERFACE || false),
     globalNamespace: (js.lang.GlobalContext.JS_GLOBAL_NAMESPACE || false)
 };
 
-// delete global clobbers (i.e. doesn't allow munging global-context.)
 try {
+    /** Delete global clobbers (try-catch for IE: doesn't allow munging global-context.) **/
     delete(js.lang.GlobalContext.JS_DEBUG);
     delete(js.lang.GlobalContext.JS_GLOBAL_CLASS);
     delete(js.lang.GlobalContext.JS_GLOBAL_INTERFACE);
     delete(js.lang.GlobalContext.JS_GLOBAL_NAMESPACE);
 } catch(e) {/* do nothing. */}
 
-// prototype munging
-// TODO: inheritance with static members?
 Function.prototype.Static = function(methods) {
+    /** Merge given dictionary into prototype of function (constructor) method was called on. **/
+    // TODO: inheritance with static members?
     js.util.objectMergeAll(this, [methods]);
     return (this);
 };
 
 Function.prototype.Implements = function(/* Interface, Interface, ... */) {
+    /** Append given interfaces to this function (constructor). **/
     this.__implements__ =
         ((this.prototype && this.prototype.constructor && this.prototype.constructor.__implements__) || []).slice();
 
@@ -111,11 +118,14 @@ Function.prototype.Implements = function(/* Interface, Interface, ... */) {
 };
 
 Function.prototype.enforces = function(/* Interface, Interface, ... */) {
+    /** Check that all given interfaces are within this function (constructor)'s 
+        list of interfaces. **/
     return (js.util.arrayContainsAll(this.__implements__, arguments));
 };
 
-// Class
+
 js.lang.Class = function(/* Parent, Parent, ... */) {
+    /** Constructor. Used to initiate creation of a constructor. **/
     var c = js.lang.Class.generateClass(js.lang.Class.classInvokedWithGlobal);
     js.lang.Class.extend(c, Array.apply([], arguments));
 
@@ -127,6 +137,7 @@ js.lang.Class.CLASS_INSTANTIATED_WITHOUT_BODY = 'Class requires a class body. ie
 js.lang.Class._extending = false;
 
 js.lang.Class.generateClass = function(globalContextCallback) {
+    /** Builds a constructor with an initializer. These functions only relay arguments to helpers. **/
     var c = function() {
         return (js.lang.Class.__constructor__.call(this, c, globalContextCallback, arguments));
     };
@@ -139,6 +150,7 @@ js.lang.Class.generateClass = function(globalContextCallback) {
 };
 
 js.lang.Class.__constructor__ = function(c, globalContextCallback, args) {
+    /** Function that gets invoked when a constructor is instantiated. **/
     if (this === js.lang.GlobalContext) {
         return (globalContextCallback.call(this, c, args));
     }
@@ -157,6 +169,7 @@ js.lang.Class.__constructor__ = function(c, globalContextCallback, args) {
 };
 
 js.lang.Class.__init__ = function(c, args) {
+    /** Shared function that should be called from child initializers. **/
     if (this instanceof Function) {
         throw new TypeError(js.lang.Class.STATIC_INIT_INVOKED_WITH_GLOBAL);
     }
@@ -166,6 +179,8 @@ js.lang.Class.__init__ = function(c, args) {
 
 // TODO: extendClasses
 js.lang.Class.extend = function(c, parents) {
+    /** Invoked after constructor has been created. Cleans up prototypal 
+        inheritance inconsistences, and maintains prototype-chain. **/
     var parent = (parents[0] || Object);
 
     js.lang.Class._extending = true;
@@ -188,6 +203,7 @@ js.lang.Class.extend = function(c, parents) {
 };
 
 js.lang.Class.extendObjects = function(c, parents) {
+    /** Merge all 'parents' into c.prototype. **/
     // TODO: this is repeated three times.. extract into generic extend!
     c.prototype = (parents[0] || {});
 
@@ -197,13 +213,15 @@ js.lang.Class.extendObjects = function(c, parents) {
 };
 
 js.lang.Class.invokeIfExists = function(context, func, args) {
-    if ((typeof(func) !== 'undefined') && (func.constructor === Function)) {
+    /** Attempt to call function if it's not a falsey value. **/
+    if (Boolean(func) && (func.constructor === Function)) {
         return (func.apply(context, (args || [])));
     }
     return (undefined);
 };
 
 js.lang.Class.mergePrototypes = function(c, parents) {
+    /** Merge all methods in prototypes of 'parents' into c.prototype **/
     for (var i = 0; i < parents.length; i++) {
         parents[i] = parents[i].prototype;
     }
@@ -211,8 +229,9 @@ js.lang.Class.mergePrototypes = function(c, parents) {
     js.util.objectMergeAll(c.prototype, parents);
 };
 
-js.lang.Class.classInvokedWithGlobal = function(c, parents) {
-    js.util.objectMergeAll(c.prototype, parents);
+js.lang.Class.classInvokedWithGlobal = function(c, methods) {
+    /** Handles dictionary of methods passed as class body. **/
+    js.util.objectMergeAll(c.prototype, methods);
 
     if ('__implements__' in c) {
         js.lang.Interface.bindStubs(c, c.__implements__);
@@ -226,6 +245,7 @@ js.lang.Class.classInvokedWithGlobal = function(c, parents) {
 
 // Interface
 js.lang.Interface = function(/* Parent, Parent, ... */) {
+    /** Represents an interface to develop to. **/
     var c = js.lang.Class.generateClass(js.lang.Interface.interfaceInvokedWithGlobal);
     js.lang.Class.extendObjects(c, Array.apply([], arguments));
     return (c);
@@ -233,8 +253,10 @@ js.lang.Interface = function(/* Parent, Parent, ... */) {
 
 js.lang.Interface.METHOD_NOT_IMPLEMENTED = "Method '{name}' not implemented; desired signature: {name}({parameters}).";
 
-// TODO: bindStubsAll?
+// TODO: bindStub?
 js.lang.Interface.bindStubs = function(c, interfaces) {
+    /** Check whether c (class) has all committed methods. Log message if absent.
+        Throw helpful error is still absent upon invocation. **/
     var proto = c.prototype;
 
     for (var i = 0, s, stubs; i < interfaces.length; i++) {
@@ -251,8 +273,8 @@ js.lang.Interface.bindStubs = function(c, interfaces) {
     }
 };
 
-// TODO: generateStubsAll?
 js.lang.Interface.generateStubs = function(stubs) {
+    /** Generate a stub out of given dictionary. **/
     for (var s in stubs) {
         if (!stubs.hasOwnProperty(s)) {
             continue;
@@ -264,6 +286,7 @@ js.lang.Interface.generateStubs = function(stubs) {
 };
 
 js.lang.Interface.generateStub = function(name, parameters) {
+    /** Generate a stub out of given values. **/
     var stub = function() {
         throw new ReferenceError(stub.__message);
     };
@@ -275,12 +298,14 @@ js.lang.Interface.generateStub = function(name, parameters) {
 };
 
 js.lang.Interface.generateStubMessage = function(name, parameters) {
+    /** Generate a stub message out of given values. **/
     return (js.lang.Interface.METHOD_NOT_IMPLEMENTED
-    .replace(/\{name\}/g, name)
-    .replace(/\{parameters\}/g, (parameters || []).join(', ')));
+        .replace(/\{name\}/g, name)
+        .replace(/\{parameters\}/g, (parameters || []).join(', ')));
 };
 
 js.lang.Interface.interfaceInvokedWithGlobal = function(c, args) {
+    /** Handles dictionary of stubs passed as class body. **/
     c.__initialized__ = true;
 
     var iface = new c();
